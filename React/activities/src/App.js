37bab37e35eff7b99e752ccedf48a6bed5fec40b
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-07-16 22:59:45 -0700

    Adding Typescript and basic component structure

--- React/activities/src/App.js
+++ React/activities/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+import GameComponent from './core/game/GameComponent';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <GameComponent/>
     </div>
   );
 }
