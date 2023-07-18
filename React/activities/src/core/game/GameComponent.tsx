49a659ccc2450f4dfbf62f650e9a0e952219a9a5
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-07-17 22:36:16 -0700

    Trying to solve build errors for docker file, looks like we'll need a webserver

--- React/activities/src/core/game/GameComponent.tsx
+++ React/activities/src/core/game/GameComponent.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Stage, Sprite, Container, Text } from '@pixi/react';
 
 export default function GameComponent() {
 
     return (
-        <div>Hello Component!</div>
+        <>
+            <div>Hello Component!</div>
+            <Stage>
+                <Sprite
+                    image="https://pixijs.io/pixi-react/img/bunny.png"
+                    x={400}
+                    y={270}
+                    anchor={{ x: 0.5, y: 0.5 }}
+                />
+
+                <Container x={400} y={330}>
+                    <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
+                </Container>
+            </Stage>
+        </>
     );
 }
