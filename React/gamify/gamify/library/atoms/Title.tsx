34c3965d6d004463c71b45f64a1213dd4a6e2dda
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-11-04 17:45:13 -0700

    Adding a Title component to the Library.

---
+++ React/gamify/gamify/library/atoms/Title.tsx
@@ -0,0 +1,23 @@
+const AllowedTags = {
+    h1:  (content: any) => <h1>{content}</h1>,
+    h2:  (content: any) => <h2>{content}</h2>,
+    h3:  (content: any) => <h3>{content}</h3>,
+    h4:  (content: any) => <h4>{content}</h4>,
+    h5:  (content: any) => <h5>{content}</h5>,
+    h6:  (content: any) => <h6>{content}</h6>,
+    p:   (content: any) => <p>{content}</p>,
+    div: (content: any) => <div>{content}</div>,
+}
+
+interface ITitleProps {
+    children: any
+    tag?: keyof typeof AllowedTags;
+};
+
+const Title = ({tag = 'h1', children}: ITitleProps) => {
+    return <>
+        {AllowedTags[tag](children)}
+    </>
+};
+
+export default Title;
